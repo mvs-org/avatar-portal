9e467567c11e3c8f6f34a6a22f4c66601dccc21c
canguruhh <sven@mutzl.org>
2019-11-25 18:39:35 +0100

    Upload function

--- functions/src/entrypoints/upload.ts
+++ functions/src/entrypoints/upload.ts
@@ -1,21 +1,33 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 const express = require('express');
 const cors = require('cors');
 
+
+import multer = require('multer');
+
+const upload = multer({ storage: multer.memoryStorage() });
+
 const uploadApi = express();
 
-uploadApi.use(cors({ origin: true }));
+uploadApi.use(cors({origin: '*'}));
 
-uploadApi.post('/api/mst/:symbol', async (req: Request, res: Response) => {
-    const symbol = req.params.symbol;
-    try {
+uploadApi.post('/api/mst/:symbol', upload.any(), (req: any, res: Response) => {
+    // try {
+        const symbol = req.params.symbol;
+
+        console.log(req.file);
+        console.log(req.files);
         res.json({
-            symbol
+            symbol,
+            file: req.file,
+            files: req.files
         })
-    } catch (error){
-        res.status(500).send('ERR_INTERNAL_ERROR')
-    }
+        // console.log('bar')
+    // } catch (error) {
+    //     console.error(error)
+    //     res.status(200).send('ERR_INTERNAL_ERROR')
+    // }
 });
 
 export {
